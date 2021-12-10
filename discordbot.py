47abd932ed65a24e3655369af1a69f0d1f66ff05
sato604 <satoberu@gmail.com>
2021-12-10 13:15:34 +0900

    update bot

--- discordbot.py
+++ discordbot.py
@@ -1,9 +1,10 @@
 from discord.ext import commands
 from os import getenv
 import traceback
+import discord
 
 bot = commands.Bot(command_prefix='/')
-
+client = discord.Client()
 
 @bot.event
 async def on_command_error(ctx, error):
@@ -16,6 +17,19 @@ async def on_command_error(ctx, error):
 async def ping(ctx):
     await ctx.send('pong')
 
+@client.event
+async def on_vc_state_update(menber , before ,after):
+    if before.channel != after.channel:
+        botRoom = client.get_channel(713740989642178573)
+        announceChs = [713740989642178574,918717105136873492]
+
+        if before.channel is not None and before.channel.id in announceChs:
+            await botRoom.send("**" + before.channel.name + "** から、__" + menber.name + "__  が抜けました！")
+        if after.channel is not None and after.channel.id in announceChs:
+            await botRoom.send("**" + after.channel.name + "** に、__" + menber.name + "__  が参加しました！")
+
+
 
 token = getenv('DISCORD_BOT_TOKEN')
 bot.run(token)
+client.run(token)
