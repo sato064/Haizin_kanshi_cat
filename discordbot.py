8ae3383f299d6ea4747fd136a57e48cb1533e419
sato604 <satoberu@gmail.com>
2021-12-16 13:21:36 +0900

    add guildid

--- discordbot.py
+++ discordbot.py
@@ -13,7 +13,7 @@ DB_PASS = getenv("DB_PASS")
 DB_NAME = "heroku_e41d4f624061a51"
 
 @client.event
-async def on_voice_state_update(menber , before ,after):
+async def on_voice_state_update(member , before ,after):
     if before.channel != after.channel:
         announceChs = [586514492481994765,764127010590949406]
         if after.channel is not None and after.channel.id in announceChs:
@@ -24,13 +24,13 @@ async def on_voice_state_update(menber , before ,after):
                 database = DB_NAME
             )
             cur = conn.cursor()
-            cur.execute("select * from users where user_id = %s",(menber.id, ))
+            cur.execute("select * from users where user_id = %s",(member.id, ))
             rows = cur.fetchall()
             if not rows:
-                cur.execute("INSERT INTO users VALUES (%s, %s ,%s)",(menber.id, menber.name,"0"))
+                cur.execute("INSERT INTO users VALUES (%s, %s ,%s ,%s)",(member.id, member.name,"0",member.guild.id ))
                 conn.commit()
-                print("new user recorded,He/She is " + menber.name)
-            cur.execute("INSERT INTO user_entertimes VALUES (%s, %s)",(menber.id, str(time.time())))
+                print("new user recorded,He/She is " + member.name)
+            cur.execute("INSERT INTO user_entertimes VALUES (%s, %s)",(member.id, str(time.time())))
             conn.commit()
             conn.close()
 
@@ -42,16 +42,16 @@ async def on_voice_state_update(menber , before ,after):
                 database = DB_NAME
             )
             cur = conn.cursor()
-            cur.execute("select * from user_entertimes where user_id = %s",(menber.id, ))
+            cur.execute("select * from user_entertimes where user_id = %s",(member.id, ))
             rows = cur.fetchall()
             enter_time = rows[0][1]
             print(enter_time)
-            cur.execute("select * from users where user_id = %s",(menber.id, ))
+            cur.execute("select * from users where user_id = %s",(member.id, ))
             staytimerows = cur.fetchall()
             stay_time = staytimerows[0][2]
             delta_stay_time = float(time.time()) - float(enter_time) + float(stay_time)
-            cur.execute("UPDATE users SET user_staytime = %s WHERE user_id = %s",(str(delta_stay_time),menber.id ))
-            cur.execute("DELETE FROM user_entertimes WHERE user_id = %s",(menber.id, ))
+            cur.execute("UPDATE users SET user_staytime = %s WHERE user_id = %s",(str(delta_stay_time),member.id ))
+            cur.execute("DELETE FROM user_entertimes WHERE user_id = %s",(member.id, ))
             conn.commit()
             conn.close()
 
